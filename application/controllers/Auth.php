39a14ed80599899989571ec772ef997f010ec551
rickynda <ultramen10@gmail.com>
2019-10-13 00:43:30 +0700

    Additional

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,11 +3,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Auth extends CI_Controller {
 
+    public function __construct(){
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
     public function index(){
-        $data['title']='Login Page';
+        $this->form_validation->set_rules('username','Username','trim|required');
+        $this->form_validation->set_rules('password','Password','trim|required');
+        if($this->form_validation->run()==false){
+            $data['title']='Login Page';
         $this->load->view('template/auth_header',$data);
         $this->load->view('auth/login');
         $this->load->view('template/auth_footer');
+        }else{
+            echo "Berhasil";
+        }
+        
 
     }
     public function register(){
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -5,7 +5,7 @@
     <!-- Outer Row -->
     <div class="row justify-content-center">
 
-      <div class="col-lg-7 mt-4">
+      <div class="col-lg-5 my-4 ">
 
         <div class="card o-hidden border-0 shadow-lg my-5">
           <div class="card-body p-0">
@@ -16,19 +16,21 @@
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                   </div>
-                  <form class="user" methode="post" action="<?= base_url('auth')?>">
+                  <form class="user" method="post" action="<?= base_url('auth')?>">
                     <div class="form-group">
-                      <input type="email" class="form-control form-control-user" name="email" id="idemail"  placeholder="Masukan Email...">
+                      <input type="text" class="form-control form-control-user" name="username" id="idusername"  placeholder="Masukan username..." value="<?= set_value('username'); ?>">
+                      <?= form_error('username', '<small class="text-danger pl-3">', '</small>'); ?>
                     </div>
                     <div class="form-group">
                       <input type="password" class="form-control form-control-user" id="idpassword" nama="password" placeholder="Password">
+                      <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
                     </div>
                     <div class="form-group">
                       
                     </div>
-                    <a href="" class="btn btn-primary btn-user btn-block">
+                    <button  type="submit" class="btn btn-primary btn-user btn-block">
                       Login
-                    </a>
+                    </button>
                     
                     
                   </form>
