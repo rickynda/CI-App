7d7f92eccdb694d2ddf0fe6d7b6222a7fca5a120
rickynda <ultramen10@gmail.com>
2019-10-14 17:17:48 +0700

    register & login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -42,8 +42,7 @@ class Auth extends CI_Controller {
                     if ($user['role_id'] == 1) {
                         redirect('admin');
                     } else {
-                        //redirect('user');
-                        echo "selamat datang". $user['name'];
+                        redirect('user');
                     }
                 } else {
                     $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Wrong password!</div>');
