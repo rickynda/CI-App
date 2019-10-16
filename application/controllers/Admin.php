b4f9825859eddbb8a330e26a1376e17079b499dd
rickynda <ultramen10@gmail.com>
2019-10-16 16:13:07 +0700

    fix admin

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -6,7 +6,7 @@ class Admin extends CI_Controller {
       public function __construct(){
         parent::__construct();
         is_logged_in();
-        check_access();
+        
       }
 
        public function index(){
